59521fb372341110273ff251e9fff8f4d62b657f
hilb4er <hilb4er@mail.ru>
2023-04-11 18:22:21 +0300

    comm

--- jenkins.groovy
+++ jenkins.groovy
@@ -9,7 +9,7 @@ pipeline {
             stage ("Build") {
                 steps
                         {
-                          sh './gradlew clean test'
+                          sh './gradlew clean apiTest'
                         }
             }
         }
