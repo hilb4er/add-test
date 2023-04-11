0ac2d85acc4c7e361ac9c72e0a8895c7e500cf1b
hilb4er <hilb4er@mail.ru>
2023-04-11 17:32:40 +0300

    comm

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,63 +1,22 @@
-node {
+pipeline {
+    agent any
 
-//    try {
-//        stage("Run tests") {
-//            parallel(
-//                    'Api Tests': {
-//                        runTestWithTag("apiTests")
-//                    },
-//                    'Ui Tests': {
-//                        runTestWithTag("uiTests")
-//                    }
-//            )
-//        }
-    try {
-        parallel getTestStages(["apiTests", "uiTests"])
-    } finally {
-        stage("Allure") {
-            generateAllure()
+    tools {
+
+        jdk "1.11"
+        gradle "8.1"
+
+
+        stages {
+            stage ("Build") {
+                step
+                        {
+                          sh './gradlew clean build'
+                        }
+            }
+
         }
     }
+
+
 }
-
-def getTestStages(testTags) {
-    def stages = [:]
-    testTags.each { tag ->
-        stages["${tag}"] = {
-            runTestWithTag(tag)
-        }
-    }
-    return stages
-}
-
-
-def runTestWithTag(String tag) {
-    try {
-        labelledShell(label: "Run ${tag}", script: "chmod +x gradlew \n./gradlew -x test ${tag}")
-    } finally {
-        echo "some failed tests"
-    }
-}
-
-def getProject(String repo, String branch) {
-    cleanWs()
-    checkout scm: [
-            $class           : 'GitSCM', branches: [[name: branch]],
-            userRemoteConfigs: [[
-                                        url: repo
-                                ]]
-    ]
-}
-
-def generateAllure() {
-    allure([
-            includeProperties: true,
-            jdk              : '',
-            properties       : [],
-            reportBuildPolicy: 'ALWAYS',
-            results          : [[path: 'build/allure-results']]
-    ])
-}
-
-
-
