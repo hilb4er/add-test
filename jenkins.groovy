90fbad7c0b6b2b03daaee8fd584cb9f4c226bcd1
hilb4er <hilb4er@mail.ru>
2023-04-11 15:15:36 +0300

    comm

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,50 +1,23 @@
-task_branch = "${TEST_BRANCH_NAME}"
-def branch_cutted = task_branch.contains("origin") ? task_branch.split('/')[1] : task_branch.trim()
-currentBuild.displayName = "$branch_cutted"
-base_git_url = "https://gitlab.com/epickonfetka/cicd-threadqa.git"
-
-
 node {
-    withEnv(["branch=${branch_cutted}", "base_url=${base_git_url}"]) {
-        stage("Checkout Branch") {
-            if (!"$branch_cutted".contains("master")) {
-                try {
-                    getProject("$base_git_url", "$branch_cutted")
-                } catch (err) {
-                    echo "Failed get branch $branch_cutted"
-                    throw ("${err}")
-                }
-            } else {
-                echo "Current branch is master"
-            }
-        }
 
-        try {
-            parallel getTestStages(["apiTests", "uiTests"])
-        } finally {
-            stage ("Allure") {
-                generateAllure()
-            }
+    try {
+        stage("Run tests") {
+            parallel(
+                    'Api Tests': {
+                        runTestWithTag("apiTests")
+                    },
+                    'Ui Tests': {
+                        runTestWithTag("uiTests")
+                    }
+            )
         }
-
-//        try {
-//            stage("Run tests") {
-//                parallel(
-//                        'Api Tests': {
-//                            runTestWithTag("apiTests")
-//                        },
-//                        'Ui Tests': {
-//                            runTestWithTag("uiTests")
-//                        }
-//                )
-//            }
-//        } finally {
-//            stage("Allure") {
-//                generateAllure()
-//            }
-//        }
+    } finally {
+        stage("Allure") {
+            generateAllure()
+        }
     }
 }
+
 
 
 def getTestStages(testTags) {
--- src/test/java/gitlabTest/UiTests.java
+++ src/test/java/gitlabTest/UiTests.java
@@ -1,70 +1,128 @@
 package gitlabTest;
 
 
-import com.codeborne.selenide.Browsers;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.testng.Tag;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.LocalFileDetector;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.logging.Logger;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static com.codeborne.selenide.Selenide.*;
 
+
 @Tag("UI")
 public class UiTests {
+  //  public static WebDriver driver;
 
     @BeforeSuite
     public static void setUp() {
-        Configuration.headless = true;
-        Configuration.browser = Browsers.CHROME;
+           Configuration.headless = true;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://www.google.com/");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+//        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+
+//        driver = new ChromeDriver();
+//
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
 
     }
 
-//    @BeforeTest
-//    public void openPage() {
-//        Selenide.open("https://www.google.com/");
+    @BeforeClass
+    public void openPage() {
+
+        //  driver.get("https://www.ya.ru/");
+       //     Selenide.open("https://www.google.com/");
+
+    }
+
+    //    @BeforeMethod
+//    protected void beforeMethod() {
+//      //  DesiredCapabilities dc = new DesiredCapabilities();
+//        try {
+//            ChromeOptions options= new ChromeOptions();
+//            RemoteWebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), options);
+//            driver.manage().window().maximize();
+//            driver.get("https://www.google.com/");
+//            WebDriverRunner.setWebDriver(driver);
+//            driver.setFileDetector(new LocalFileDetector());
+//
+//
+//        } catch (MalformedURLException ex ){
+//            throw  new RuntimeException();
+//        }
+//
 //    }
+    private void assertAnswer(String value, String s) {
+        $x("//input[@name='q']").shouldBe(Condition.visible, Duration.ofSeconds(5)).sendKeys(value + "=" + Keys.ENTER);
 
-    @BeforeMethod
-    protected void beforeMethod() {
-      //  DesiredCapabilities dc = new DesiredCapabilities();
-        try {
-            ChromeOptions options= new ChromeOptions();
-            RemoteWebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), options);
-            driver.manage().window().maximize();
-            driver.get("https://www.google.com/");
-            WebDriverRunner.setWebDriver(driver);
-            driver.setFileDetector(new LocalFileDetector());
-
-
-        } catch (MalformedURLException ex ){
-            throw  new RuntimeException();
-        }
-
-    }
-    private void assertAnswer(String value) {
-        $x("//input[@name='q']").sendKeys(value + "=" + Keys.ENTER);
         String answer = $x("//span[@id='cwos']").getText();
+        System.out.println(answer);
         Assert.assertEquals("4", answer);
     }
 
-    @AfterSuite
-    protected  void after () {
-        closeWebDriver();
+    private void assertans(String value, String s) {
+     //   driver.get("https://www.ya.ru/");
+        $(".body__content .search3__inner input").sendKeys(value,s + "=" + Keys.ENTER);
+        int i = 0;
     }
-    @Test
-    public void calcPlusTest() {
-        assertAnswer("2+2");
+
+    @AfterSuite
+    protected void after() {
+//        driver.close();
+          closeWebDriver();
+    }
+
+  //
+   @Parameters({"param", "param2"})
+    @Test()
+    public void calcPlusTest(String param, String param2) {
+       assertAnswer(param, param2 + "+2");
+    }
+
+   // @Parameters({"param"})
+    @Test(dataProvider="SearchProvider")
+    public void Search(String param, String param2) throws InterruptedException {
+
+
+        System.out.println();
+
+
+        assertAnswer(param, param2 + "+2");
+
+    }
+
+
+    @DataProvider(name="SearchProvider")
+    public Object [][] getData() {
+        return new Object[][]
+                {
+                        {"5","6"},
+                        {"8","7"},
+                        {"9","1"}
+                };
+    }
+    @annot(name = "gg")
+    public class clss {
+
+        @annot(name = "gg")
+        public  void  aVoid () {
+
+        }
+
+
     }
 //
 //    @Test
